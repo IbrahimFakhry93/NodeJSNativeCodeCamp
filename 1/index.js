82d44aabf09e2c74bae5f875cdc3b2afe04b6b9f
Ibrahim <ibrahim.fakhry93@yahoo.com>
2025-04-05 08:44:07 +0200

    variables

--- 1/index.js
+++ 1/index.js
@@ -1,5 +1,16 @@
-const florFah = 75;
-const floridaCel = (florFah - 32) * (5 / 9);
-console.log(floridaCel);
+// const florFah = 75;
+// const floridaCel = (florFah - 32) * (5 / 9);
+// console.log(floridaCel);
 
 //* comment
+// Initialize the variable
+var x = 5;
+//* The current value of x (5) is assigned to the variable result first.
+var result = x++;
+
+//* After the assignment, the value of x is incremented by 1.
+//* At this point, result holds the value 5, and x becomes 6.
+
+// Output the values
+console.log("Value of result: " + result); // Output: 5
+console.log("Value of x: " + x); // Output: 6
--- 1/notes.js
+++ 1/notes.js
@@ -1,6 +1,95 @@
 //* to run index.js
 //* go to terminal: node index.js
 
+//! Variables:
+
+//* Before any processing, all variables must be stored in the memory called RAM (Random Access Memory).
+//* The programming language initiates this request, which is then handled by the operating system.
+//* The programming language, in collaboration with the operating system, further interacts with the hardware
+//* to ensure that input or output data is stored in the memory (RAM) as required.
+
+//* steps to create variable with value:
+//* 1. declaration" var data
+//* 2. initialization: var data = 5;
+//? or:
+//* var data;
+//* data = 5;  intialization will be in a place in the middle of the code not right after the declaration
+
+//? or:
+//* var x , y, strData;
+//* x = 2;
+//* y = 3;
+//* strData = ' this is string '
+
+//? example:
+//* var x , y, result;   Declaration
+//* x = 2;               Assignment
+//* y = 3;               Assignment
+//* result = x + y;      usage
+
+//? print value of these variables
+// console.log(any_valid_line_of_code);
+// console.log("text");
+// console.log(variable_name);
+// console.log(2 + 3);
+// console.log(call_for_another_function);
+// console.log(and_more);
+
+//? valid forms for variable name:
+//* var data_20 = 5;
+//* var data_first =5;
+//* var _data=5;
+//* var $data=5;
+
+//? var names are case sensitive:
+//* var data=5;
+//* var DATA=5;
+
+//*=============================================================================================================================
+
+//! Variables – hands-on
+
+var data = 5; //* declaration
+data = 10; //* assignment
+console.log(data); //* usage
+
+//*=============================================================================================================================
+
+//! Operators – Arithmetic
+
+//~ x++ post-increment operator:
+
+// Initialize the variable
+var x = 5;
+
+//* The current value of x (5) is assigned to the variable result first.
+var result = x++;
+
+//* After the assignment, the value of x is incremented by 1.
+//* At this point, result holds the value 5, and x becomes 6.
+
+// Output the values
+console.log("Value of result: " + result); // Output: 5
+console.log("Value of x: " + x); // Output: 6
+
+//~ same for x--
+
+//~ ++x pre-increment operator
+
+// Initialize the variable
+var x = 5;
+
+//* The value of x is incremented by 1 first.
+//* After the increment, the new value of x (6) is assigned to the variable result.
+var result = ++x;
+
+// Output the values
+console.log("Value of result: " + result); // Output: 6
+console.log("Value of x: " + x); // Output: 6
+
+//~ same for --x
+
+//*=============================================================================================================================
 //! For loop:
 
 //& Title: Execution Steps of a For Loop in JavaScript
