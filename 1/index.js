47bacace80e7de4bd2d87a72718208e675416cce
Ibrahim <ibrahim.fakhry93@yahoo.com>
2025-04-05 13:18:12 +0200

    operators + conditions

--- 1/index.js
+++ 1/index.js
@@ -2,15 +2,43 @@
 // const floridaCel = (florFah - 32) * (5 / 9);
 // console.log(floridaCel);
 
-//* comment
-// Initialize the variable
-var x = 5;
-//* The current value of x (5) is assigned to the variable result first.
-var result = x++;
+// //* comment
+// // Initialize the variable
+// var x = 5;
+// //* The current value of x (5) is assigned to the variable result first.
+// var result = x++;
 
-//* After the assignment, the value of x is incremented by 1.
-//* At this point, result holds the value 5, and x becomes 6.
+// //* After the assignment, the value of x is incremented by 1.
+// //* At this point, result holds the value 5, and x becomes 6.
 
-// Output the values
-console.log("Value of result: " + result); // Output: 5
-console.log("Value of x: " + x); // Output: 6
+// // Output the values
+// console.log("Value of result: " + result); // Output: 5
+// console.log("Value of x: " + x); // Output: 6
+
+// var x = 5;
+// var y = 18;
+
+// console.log(`${x}`);
+// console.log(`${y}`);
+
+// console.log("after swap");
+
+// function swap(x, y) {
+//   var temp;
+
+//   temp = x;
+
+//   x = y;
+
+//   y = temp;
+
+//   console.log(`${x}`);
+//   console.log(`${y}`);
+// }
+
+// swap(x, y);
+
+let var1 = 5;
+let var2 = "5";
+if (var1 !== var2) console.log("The condition is true");
+else console.log("The condition is false");
